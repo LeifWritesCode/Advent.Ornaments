378715fd47994e6c878544806022489e767b1430
Leif <24778021+LeifWritesCode@users.noreply.github.com>
2022-12-19 18:23:38 +0100

    Create OpticalCharacterRecognition.cs
    Add beginnings of an OCR system for image based outputs where image is of a low density display

---
+++ Ornaments/Text/OpticalCharacterRecognition.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+
+namespace Ornaments.Text;
+
+internal interface IOpticalCharacterRecognitionStategy
+{
+    bool TryRead(string[] lines, out string text);
+}
+
+public enum OcrStrategy
+{
+    FourBySixWithSpace,
+}
+
+public class OpticalCharacterRecognition : IOpticalCharacterRecognitionStategy
+{
+    private readonly IOpticalCharacterRecognitionStategy opticalCharacterRecognitionStategy;
+
+    private OpticalCharacterRecognition(IOpticalCharacterRecognitionStategy opticalCharacterRecognitionStategy)
+    {
+        this.opticalCharacterRecognitionStategy = opticalCharacterRecognitionStategy;
+    }
+
+    public bool TryRead(string[] lines, out string text)
+    {
+        return opticalCharacterRecognitionStategy.TryRead(lines, out text);
+    }
+
+    public static bool TryRead(OcrStrategy ocrStrategy, string[] lines, out string text)
+    {
+        var ocr = new OpticalCharacterRecognition(ocrStrategy switch
+        {
+            OcrStrategy.FourBySixWithSpace => new OcrStrategy4x6(),
+            _ => throw new UnreachableException()
+        });
+        return ocr.TryRead(lines, out text);
+    }
+}
+
+public class OcrStrategy4x6 : IOpticalCharacterRecognitionStategy
+{
+    private const int charWidth = 5;
+    private const int charHeight = 6;
+    private readonly IDictionary<string, char> characterMap = new Dictionary<string, char>()
+    {
+        {
+            """
+            .##..
+            #..#.
+            #..#.
+            ####.
+            #..#.
+            #..#.
+            """,
+            'A'
+        }
+    };
+
+    internal OcrStrategy4x6() { }
+
+    public bool TryRead(string[] lines, out string text)
+    {
+        text = string.Empty;
+        if (lines is null || lines.Length != charHeight)
+            return false;
+
+        var charactersInArray = lines.First().Length / charWidth;
+        for (var i = 0; i < charactersInArray; i++)
+        {
+            var candidate = string.Join(string.Empty, lines.Select(x => x[i..(i + charWidth)]));
+            if (!characterMap.TryGetValue(candidate, out char value))
+            {
+                return false;
+            }
+            text += value;
+        }
+        return true;
+    }
+}
